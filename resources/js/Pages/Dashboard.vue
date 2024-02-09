7dc936f9af040412f2c08edee045c17ee8f6e46e
Vari <variares7921@aol.com>
2024-02-09 01:40:58 +0100

    Initial Inertia Setup and removed Sanctum.

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import { Head } from '@inertiajs/vue3'
+
+defineProps({
+  test: String,
+})
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello {{ test }} welcome to your first Inertia app!</p>
+</template>
+
+<style scoped>
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue";
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,

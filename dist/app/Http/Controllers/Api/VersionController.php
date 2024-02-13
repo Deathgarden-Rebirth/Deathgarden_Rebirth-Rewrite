b39781e399a3b325a5745e9cbd8e55f2adf89ef9
Vari <variares7921@aol.com>
2024-02-13 01:28:03 +0100

    Added some Versioning endpoints

---
+++ dist/app/Http/Controllers/Api/VersionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class VersionController extends Controller
+{
+    public function getLatestClientData()
+    {
+        return json_encode(['LatestSupportedVersion' => 'te-18f25613-36778-ue4-374f864b']);
+    }
+
+    public function getLatestContentVersion(string $version)
+    {
+        $supportedVersion = match($version) {
+            '0' => 'dev030',
+            '2.0' => 'dev020',
+            '2.2' => 'te-23ebf96c-27498-ue4-7172a3f5',
+            '2.5' => 'te-40131b9e-33193-ue4-fbccc218',
+            '3.0' => 'dev030',
+            default => 'te-18f25613-36778-ue4-374f864b'
+        };
+
+        return ['LatestSupportedVersion' => $supportedVersion];
+    }
+}
--- dist/routes/deathgardenApi.php
+++ dist/routes/deathgardenApi.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\Auth\SteamAuthController;
 use App\Http\Controllers\Api\Eula\EulaConsentController;
+use App\Http\Controllers\Api\VersionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('auth/provider/steam/login',[SteamAuthController::class, 'login']);
+
+    Route::get('config/VER_LATEST_CLIENT_DATA', [VersionController::class, 'getLatestClientData']);
+    Route::get('utils/contentVersion/latest/{version}', [VersionController::class, 'getLatestContentVersion']);
 
     Route::middleware('api.auth')->group(function () {
         Route::get('consent/eula2', [EulaConsentController::class, 'get'])->name(EulaConsentController::class);

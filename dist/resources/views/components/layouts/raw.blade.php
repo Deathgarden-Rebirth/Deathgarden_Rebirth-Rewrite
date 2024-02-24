d29b19f60209031d9cd6e92698214bcad7c1052e
Patrik Leifert <patrikleifert@hotmail.com>
2024-02-24 16:27:37 +0100

    Add TailwindCSS and create basic layout

--- dist/resources/views/components/layouts/raw.blade.php
+++ dist/resources/views/components/layouts/raw.blade.php
@@ -4,9 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @inertiaHead
 </head>
 <body>
-@inertia
+{{ $slot }}
 </body>
 </html>

d0a5e2365256c735e84e1d25d059ae85097ba82b
Vari <variares7921@aol.com>
2024-02-25 00:08:17 +0100

    Chenges to access logger

--- dist/app/Http/Middleware/AccessLogger.php
+++ dist/app/Http/Middleware/AccessLogger.php
@@ -16,12 +16,13 @@ class AccessLogger
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(!Str::contains($request->userAgent(), 'TheExit'))
+        if (!Str::contains($request->userAgent(), 'TheExit'))
             return $next($request);
+
+        $response = $next($request);
 
         $log = new stdClass();
         $log->method = $request->method();
@@ -32,46 +33,32 @@ class AccessLogger
 
         $channels = ['dg_requests'];
 
-        try {
-            $response = $next($request);
+        if (!Session::has('sessionLogConfig')) {
+            $user = Auth::user();
+            $username = $user?->last_known_username ?? '';
+            $logConfig = [
+                'driver' => 'single',
+                'path' => storage_path('logs/sessions/' . $username . '_' . Str::substr(Session::getId(), 0, 12) . '.log')
+            ];
+            Session::put('sessionLogConfig', $logConfig);
+        } else
+            $logConfig = Session::get('sessionLogConfig');
 
-            if(!Session::has('sessionLogConfig')) {
-                $user = Auth::user();
-                $username = $user?->last_known_username ?? '';
-                $logConfig = [
-                    'driver' => 'single',
-                    'path' => storage_path('logs/sessions/'.$username.'_'.Str::substr(Session::getId(), 0, 12).'.log')
-                ];
-                Session::put('sessionLogConfig', $logConfig);
-            }
-            else
-                $logConfig = Session::get('sessionLogConfig');
+        $logChannel = Log::build($logConfig);
 
-            $logChannel = Log::build($logConfig);
+        $channels[] = $logChannel;
 
-            $channels[] = $logChannel;
+        $log->response = new stdClass();
+        $log->response->statusCode = $response->getStatusCode();
+        $log->response->body = json_decode($response->getContent());
 
-            $log->response = new stdClass();
-            $log->response->statusCode = $response->getStatusCode();
-            $log->response->body = json_decode($response->getContent());
+        $logMessage = $log->method . ' ' . $log->url . "\n" . json_encode($log);
 
-            $logMessage = $log->method.' '.$log->url."\n".json_encode($log);
+        if ($response->isSuccessful())
+            Log::stack($channels)->info($logMessage);
+        else
+            Log::stack($channels)->warning($logMessage);
 
-            if($response->isSuccessful())
-                Log::stack($channels)->info($logMessage);
-            else
-                Log::stack($channels)->warning($logMessage);
-
-            return $response;
-        } catch (\Exception $e) {
-            $log->exception = new stdClass();
-            $log->exception->code = $e->getCode();
-            $log->exception->message = $e->getMessage();
-            $log->exception->trace = $e->getTrace();
-
-            $logMessage = $log->method.' '.$log->url."\n".json_encode($log);
-            Log::stack($channels)->error($logMessage);
-            throw $e;
-        }
+        return $response;
     }
 }

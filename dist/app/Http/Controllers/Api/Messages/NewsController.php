2f2b9288575973bc07ac357bb17498c04f3c59b8
Vari <variares7921@aol.com>
2024-03-15 23:39:03 +0100

    Fixed game crash when embeddded images in gamenews are null, needs to be a string.
    Fixed Game news where clause.

--- dist/app/Http/Controllers/Api/Messages/NewsController.php
+++ dist/app/Http/Controllers/Api/Messages/NewsController.php
@@ -8,15 +8,17 @@ use App\Http\Responses\Api\Messages\News\GameNews;
 use App\Http\Responses\Api\Messages\News\GameNewsResponse;
 use App\Http\Responses\Api\Messages\News\GameNewsTranslation;
 use App\Models\Game\Messages\News;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Builder;
 
 class NewsController extends Controller
 {
     public function getGameNews(GameNewsMessagesRequest $request)
     {
         $newsCollection = News::where('message_type', '=', $request->messageType->value)
-            ->where('max_player_level', '>=', $request->playerLevel)
-            ->orWhereNull('max_player_level')
+            ->where(function (Builder $query) use ($request) {
+                $query->where('max_player_level', '>=', $request->playerLevel)
+                    ->orWhereNull('max_player_level');
+            })
             ->orderBy('created_at', $request->sortDescending ? 'desc' : 'asc')
             ->get();
 
@@ -34,9 +36,9 @@ class NewsController extends Controller
             $newsResponse->redirectMode = $news->redirect_mode;
             $newsResponse->redirectItem = $news->redirect_item;
             $newsResponse->redirectUrl = $news->redirect_url;
-            $newsResponse->embeddedBackgroundImage = $news->background_image;
-            $newsResponse->embeddedInGameNewsBackgroundImage = $news->in_game_news_background_image;
-            $newsResponse->embeddedInGameNewsThumbnailImage = $news->in_game_news_thumbnail;
+            $newsResponse->embeddedBackgroundImage = $news->background_image ?? 'null';
+            $newsResponse->embeddedInGameNewsBackgroundImage = $news->in_game_news_background_image ?? 'null';
+            $newsResponse->embeddedInGameNewsThumbnailImage = $news->in_game_news_thumbnail ?? 'null';
             $newsResponse->fromDate = $news->from_date;
             $newsResponse->toDate = $news->to_date;
             $newsResponse->displayXTimes = $news->display_x_times;

e2342578e69bf68a3a32de1adbe8e978e694ca4d
Vari <variares7921@aol.com>
2024-04-28 16:41:01 +0200

    Added maintenance Mode which can be enabled by creating a file named .maintenance inside the storage/app directory.
    When on, users get shown a maintenance page, except if you have the VIEW_MAINTENANCE permission. The admin area works either way and you get shown a small 'maintenance mode' watermark in the bottom right corner.

--- dist/resources/views/components/layouts/app.blade.php
+++ dist/resources/views/components/layouts/app.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title ?? 'Deathgarden Rebirth' }}</title>
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
@@ -20,8 +20,24 @@
         <x-alerts.warning heading="Warning">{!! Session::get('alert-warning') !!}</x-alerts.warning>
     @endif
 </div>
+@php
+    use App\Enums\Auth\Permissions;
+    use Illuminate\Support\Facades\Auth;
 
-{{ $slot }}
+    $maintenanceMode = \App\Helper\AppHelper\App::isInMaintenanceMode();
+    $showMaintenancePage = request()->route()->getPrefix() !== 'admin' &&
+        !(Auth::user() !== null && Auth::user()->can(Permissions::VIEW_MAINTENANCE->value));
+@endphp
+@if($maintenanceMode && $showMaintenancePage)
+    <x-misc.maintenance/>
+@else
+    @if($maintenanceMode)
+        <div class="fixed bottom-0 right-0">
+            <span class="text-gray-500/80">Maintenance mode active</span>
+        </div>
+    @endif
+    {{ $slot }}
+@endif
 </body>
 <footer>
     @stack('footer')
---
+++ dist/resources/views/components/misc/maintenance.blade.php
@@ -0,0 +1,17 @@
+<div class="flex flex-col items-center text-center absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
+    <img src="{{ asset('img/logos/DG_BH_Logo.png') }}"
+         alt="Deathgarden Bloodharves Logo"
+    >
+
+    <h1 class="text-7xl mb-6 mt-[-1em]">Rebirth</h1>
+    <p class="text-4xl">
+        This page is currently under construction. <br>
+        Keep up to date on our Discord in the meantime!
+    </p>
+
+    <a href="https://tinyurl.com/chronosdiscord" target="_blank">
+        <div>
+            <x-icons.discord class="w-24 mt-6 hover:fill-indigo-600" />
+        </div>
+    </a>
+</div>

3b4737b3158e256b83e3e5b04a19115e7ea8de45
Vari <variares7921@aol.com>
2024-08-21 00:31:54 +0200

    Added footer to web layout

--- dist/resources/views/components/layouts/web.blade.php
+++ dist/resources/views/components/layouts/web.blade.php
@@ -1,8 +1,14 @@
 <x-layouts.app>
     <div class="h-screen flex bg-inherit">
         <x-web.sidebar/>
-        <div class="row-span-2 h-full w-full px-4 md:px-16 lg:px-32 bg-inherit overflow-x-auto">
-            {{ $slot }}
+        <div class="row-span-2 h-full w-full bg-inherit overflow-x-auto flex flex-col justify-between ">
+            <div class="px-4 md:px-16 lg:px-32">
+                {{ $slot }}
+            </div>
+
+            <div class="w-full [background:linear-gradient(to_right,transparent,theme(colors.gray.900)_40%,theme(colors.gray.900)_60%,transparent)] border-t-2 border-web-main">
+                <x-web.footer />
+            </div>
         </div>
     </div>
 </x-layouts.app>
---
+++ dist/resources/views/components/web/footer.blade.php
@@ -0,0 +1,31 @@
+<div class="flex flex-col justify-center container mx-auto text-center py-4">
+    <div class="flex gap-6 justify-center">
+        <a href="https://x.com/dgrebirth_/"
+           class="hover:text-web-main transition-colors"
+           title="X formally known as twitter"
+        >
+            <x-icons.twitter-x class="size-8"/>
+        </a>
+        <a href="https://twitch.com/dgrebirth_/"
+           class="hover:text-web-main transition-colors"
+           title="Twitch"
+        >
+            <x-icons.twitch class="size-8"/>
+        </a>
+        <a href="https://discord.gg/7MqudBGyyp/"
+           class="hover:text-web-main transition-colors"
+           title="Discord"
+        >
+            <x-icons.discord class="size-8"/>
+        </a>
+        <a href="mailto:contact@playdeathgarden.live"
+           class="hover:text-web-main transition-colors"
+           title="Email"
+        >
+            <x-icons.mail class="size-8"/>
+        </a>
+    </div>
+    <span class="text-xs mt-4">
+            2018-2024 and BEHAVIOUR, DEATHGARDEN, and other related trademarks and logos belong to Behaviour Interactive Inc. All rights reserved. The Rebirth Team is not affiliated with Behaviour Interactive Inc. and is a free to play project.
+    </span>
+</div>

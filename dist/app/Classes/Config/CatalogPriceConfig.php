6bcbe5f92504a819a6c78751b93d92b534fa837a
Miraak <k.jendou@gmail.com>
2024-10-01 10:12:08 +0200

    Initial 1.2.0 Commit
    - Prepared Item swaps
    - Fixed Config issues
    - Set Level 10 Weapons as "Default Weapons" for Inquisitor as Test
    - Updated Catalog
    - Updated Catalog Prices

--- dist/app/Classes/Config/CatalogPriceConfig.php
+++ dist/app/Classes/Config/CatalogPriceConfig.php
@@ -132,48 +132,56 @@ class CatalogPriceConfig
     const PERKS_COST = [
         1 => [
             'CurrencyB' => 500,
+            'CurrencyC' => 125,
         ],
         2 => [
             'CurrencyB' => 750,
+            'CurrencyC' => 750,
         ],
         3 => [
             'CurrencyB' => 1000,
+            'CurrencyC' => 1000,
         ],
         4 => [
             'CurrencyB' => 1250,
+            'CurrencyC' => 1250,
         ],
         5 => [
             'CurrencyB' => 1500,
+            'CurrencyC' => 1500,
         ],
         6 => [
-            'CurrencyA' => 425,
+            'CurrencyA' => 500,
             'CurrencyB' => 1300,
+            'CurrencyC' => 1300,
         ],
         7 => [
-            'CurrencyA' => 525,
+            'CurrencyA' => 750,
             'CurrencyB' => 1400,
+            'CurrencyC' => 1400,
         ],
         8 => [
-            'CurrencyA' => 625,
+            'CurrencyA' => 875,
             'CurrencyB' => 1500,
+            'CurrencyC' => 1500,
         ],
         9 => [
-            'CurrencyA' => 725,
+            'CurrencyA' => 1000,
             'CurrencyB' => 1600,
+            'CurrencyC' => 1600,
         ],
         10 => [
-            'CurrencyA' => 825,
+            'CurrencyA' => 1250,
             'CurrencyB' => 1700,
+            'CurrencyC' => 1700,
         ],
     ];
 
     const SKINS_COST = [
-        ItemQuality::Basic->value => ['CurrencyC' => 2500],
-        ItemQuality::Specialized->value => ['CurrencyC' => 3500],
-        ItemQuality::Rare->value => ['CurrencyC' => 4500],
-        ItemQuality::Superior->value => ['CurrencyC' => 5500],
-        ItemQuality::Epic->value => ['CurrencyC' => 6250],
-        ItemQuality::Ultra->value => ['CurrencyC' => 7500],
+        ItemQuality::Basic->value => ['CurrencyA' => 1250, 'CurrencyC'=> 3500],
+        ItemQuality::Specialized->value => ['CurrencyA' => 2500, 'CurrencyC' => 5500],
+        ItemQuality::Superior->value => ['CurrencyA' => 2500, 'CurrencyC' => 5500],
+        ItemQuality::Epic->value => ['CurrencyA' => 3250, 'CurrencyC' => 6250],
     ];
 
     public static function GetCategoryPriceForLevel(CatalogPriceCategory $category, int|string $level): array

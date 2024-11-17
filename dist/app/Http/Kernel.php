99ed53b02ac9e16448ac92be5e8ccb37f139965c
Vari <variares7921@aol.com>
2024-11-17 02:56:35 +0100

    Laravel 11 Upgrade: - upgrade laravel to ^11.0 - removed ancient inertia packages - fixed one small issue with carbon that arose after the upgrade

--- dist/app/Http/Kernel.php
+++ dist/app/Http/Kernel.php
@@ -3,7 +3,6 @@
 namespace App\Http;
 
 use App\Http\Middleware\AccessLogger;
-use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -39,7 +38,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            HandleInertiaRequests::class,
         ],
 
         'api' => [

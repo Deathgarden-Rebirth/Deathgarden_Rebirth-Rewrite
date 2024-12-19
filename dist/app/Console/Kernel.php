e39a17162d4d61133161943e8d09b0f9a9e5d91f
Vari <variares7921@aol.com>
2024-12-19 23:39:25 +0100

    Increase Matchmaking interval to reduce the chance of 1v4 games until new matchmaking is here.

--- dist/app/Console/Kernel.php
+++ dist/app/Console/Kernel.php
@@ -13,8 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('model:prune')->daily();
-        $schedule->command('matchmaking:process')->everyFiveSeconds();
-        $schedule->command('matchmaking:cleanup')->everyFifteenSeconds();
+        $schedule->command('matchmaking:process')->everyTwentySeconds();
+        $schedule->command('matchmaking:cleanup')->everyThirtySeconds();
         $schedule->command('app:generate-timed-challenges')->daily();
     }
 

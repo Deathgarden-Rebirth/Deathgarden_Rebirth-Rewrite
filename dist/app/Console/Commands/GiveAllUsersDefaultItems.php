2181c2d8ce01d92af6e6f868c5b733084af618a5
Vari <variares7921@aol.com>
2024-12-19 21:38:50 +0100

    Added command to add the default items to all default users.

---
+++ dist/app/Console/Commands/GiveAllUsersDefaultItems.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Classes\Character\CharacterItemConfig;
+use App\Enums\Game\Characters;
+use App\Helper\Uuid\UuidHelper;
+use App\Models\User\PlayerData;
+use Illuminate\Console\Command;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Log;
+
+class GiveAllUsersDefaultItems extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:give-all-users-default-items';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $playerDatas = PlayerData::all();
+
+        foreach ($playerDatas as $playerData) {
+            foreach (Characters::cases() as $case) {
+                $this->info('Adding items for user ' . $playerData->user_id . ' for  Character ' . $case->value);
+                $configClass = $case->getCharacter()->getItemConfigClass();
+                $this->addItemsToUser($playerData, $configClass);
+            }
+        }
+    }
+
+    /**
+     * @param PlayerData $user
+     * @param class-string<CharacterItemConfig> $config
+     * @return void
+     */
+    public function addItemsToUser(PlayerData &$playerData, string $config): void {
+        $itemIds = [
+            ...$config::getDefaultEquippedBonuses(),
+            ...$config::getDefaultEquippedWeapons(),
+            ...$config::getDefaultEquipment(),
+            ...$config::getDefaultPowers(),
+            ...$config::getDefaultEquippedPerks(),
+        ];
+
+        $itemIds = UuidHelper::convertFromHexToUuidCollecton($itemIds, true);
+        try {
+            $playerData->inventory()->syncWithoutDetaching($itemIds);
+        } catch (QueryException $e) {
+            Log::channel('single')->error($e->getMessage());
+            $this->error('Exception: ' . $e->getMessage());
+            $this->error("Failed to add items to inventory: " . json_encode($itemIds, JSON_PRETTY_PRINT));
+        }
+    }
+}

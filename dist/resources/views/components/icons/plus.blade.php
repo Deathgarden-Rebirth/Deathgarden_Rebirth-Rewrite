25854553c2dcf3cfa8f2904410ba7d6ce3020a01
Shaggy84675 <admin@RainbowShaggy.com>
2025-08-02 14:56:42 +0200

    Modify plus icon for consistency
    Signed-off-by: Shaggy84675 <admin@RainbowShaggy.com>

--- dist/resources/views/components/icons/plus.blade.php
+++ dist/resources/views/components/icons/plus.blade.php
@@ -1,3 +1,3 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {{ $attributes }}>
-    <path fill-rule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" {{ $attributes }}>
+    <path d="M5 12h14"/><path d="M12 5v14"/>
 </svg>

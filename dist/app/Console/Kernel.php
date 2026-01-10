8a05472d9b3c1a4648901d3cb60317b559651e78
Miraak <k.jendou@gmail.com>
2026-01-10 21:45:32 +0100

    Disabled the currency modifier chron job for NA + added the current live catalog

--- dist/app/Console/Kernel.php
+++ dist/app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('matchmaking:cleanup')->everyThirtySeconds();
         $schedule->command('app:generate-timed-challenges')->daily();
         $schedule->command('app:cleanup-logs')->daily();
-        $schedule->command('app:set-currency-modifiers')->hourlyAt(1);
+        // $schedule->command('app:set-currency-modifiers')->hourlyAt(1);
     }
 
     /**
